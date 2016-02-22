5d21af7d1a3d840539524d2a40426c5a74912980
Paul Morris <pmorris2012@fau.edu>
2016-02-22 12:53:15 -0500

    added rover library

---
+++ rover/byteutils.py
@@ -0,0 +1,24 @@
+"""
+Python methods for converting bytestrings to integers.
+"""
+
+import struct
+import sys
+
+
+def dump_bytes(bytes):
+    for c in bytes:
+        sys.stdout.write('%02x ' % ord(c))
+    sys.stdout.write('\n')
+
+
+def bytes_to_int(bytes, offset):
+    return struct.unpack('i', bytes[offset:offset + 4])[0]
+
+
+def bytes_to_uint(bytes, offset):
+    return struct.unpack('I', bytes[offset:offset + 4])[0]
+
+
+def bytes_to_short(bytes, offset):
+    return struct.unpack('h', bytes[offset:offset + 2])[0]
